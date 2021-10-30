a60ea79c594853b0e60ee3416fec24a465ab674e
Dinesh Kumar <55187704+dk-dinesh@users.noreply.github.com>
2021-10-30 23:54:09 +0530

    Kadane's Algorithm

--- 13.cpp
+++ 13.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution{
     public:
     // arr: input array
-    // n: size of input array
+    // n: size of array
     //Function to find the sum of contiguous subarray with maximum sum.
     long long maxSubarraySum(int arr[], int n)
     {
